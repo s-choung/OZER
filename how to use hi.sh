ee11a6bd0976546259fde25528d51748a1d5747e
Seokhyun Choung <71485567+s-choung@users.noreply.github.com>
2022-05-13 17:14:46 +0900

    Create how to use hi.sh

---
+++ how to use hi.sh
@@ -0,0 +1,9 @@
+How to use hi.sh
+
+
+1) prepare potcars folder at your home
+2) preapre a folder with name "0" and put 1~n folders  which will have structure calculations of each dopants.
+sh hi.sh 
+then 
+1_OH 2_O 3_OOH ~~ 6_O2+O folder will be generated with 1~n dopants in it.
+then you just run them!! 
